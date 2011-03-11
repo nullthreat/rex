b70757e2f0416f458f9cdfc9caa65df926b18bbb
Jacob Hammack <jacob.hammack@hammackj.com>
2011-03-11 17:38:56 -0600

    Updated for Revision 11938

---
+++ lib/rex/proto.rb
@@ -0,0 +1,13 @@
+require 'rex/proto/http'
+require 'rex/proto/smb'
+require 'rex/proto/ntlm'
+require 'rex/proto/dcerpc'
+require 'rex/proto/drda'
+
+module Rex
+module Proto
+
+attr_accessor :alias
+
+end
+end
